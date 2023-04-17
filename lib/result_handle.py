3c61da0bb81af7973bdc3006ed2273b91eccd57d
HDEnt <107353808+HDEnt327@users.noreply.github.com>
2023-04-17 19:03:49 +0800

    Add files via upload

---
+++ lib/result_handle.py
@@ -0,0 +1,16 @@
+from khl import Bot, Message, PublicTextChannel, api, MessageTypes
+
+async def task_complete(msg: Message):
+    ...
+
+
+
+async def task_cancelled(msg: Message):
+    ...
+
+
+
+async def task_timeout(msg: Message):
+    ...
+
+    
